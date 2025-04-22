9c4f4413e33040c6cf599e041e4aa59b92b2d304
dyw770 <dyw770@qq.com>
2025-04-22 22:36:31 +0800

    增加db模块的自动配置

---
+++ auth-db/src/main/java/cn/dyw/auth/db/configuration/AuthJdbcAutoConfiguration.java
@@ -0,0 +1,17 @@
+package cn.dyw.auth.db.configuration;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dyw770
+ * @since 2025-04-22
+ */
+@Configuration
+@MapperScan(basePackages = "cn.dyw.auth.db.mapper")
+@ComponentScan(basePackages = "cn.dyw.auth")
+public class AuthJdbcAutoConfiguration {
+
+
+}
