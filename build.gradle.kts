77559b35828b60ca61311d15db6ed86cc8bf1063
dyw770 <dyw770@qq.com>
2025-04-30 18:17:20 +0800

    修改子项目配置，排除非java项目

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,30 @@ allprojects {
     version = "0.0.1-SNAPSHOT"
 }
 
+val nonJavaProjects = listOf("auth-ui")
+
 subprojects {
-    apply {
-        plugin("java")
-        plugin("io.spring.dependency-management")
+    if (!nonJavaProjects.contains(project.name)) {
+        apply {
+            plugin("java")
+            plugin("io.spring.dependency-management")
+        }
+
+        dependencyManagement {
+            dependencies {
+                imports {
+                    mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+                    mavenBom("com.baomidou:mybatis-plus-bom:3.5.11")
+                }
+
+                dependency("com.alibaba:druid-spring-boot-3-starter:1.2.22")
+
+                dependency("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+
+                dependency("com.github.therapi:therapi-runtime-javadoc:0.15.0")
+                dependency("com.github.therapi:therapi-runtime-javadoc-scribe:0.15.0")
+            }
+        }
     }
 
     // 配置java版本
@@ -46,19 +66,4 @@ subprojects {
         options.compilerArgs.add("-parameters")
     }
     
-    dependencyManagement {
-        dependencies {
-            imports {
-                mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
-                mavenBom("com.baomidou:mybatis-plus-bom:3.5.11")
-            }
-            
-            dependency("com.alibaba:druid-spring-boot-3-starter:1.2.22")
-            
-            dependency("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
-            
-            dependency("com.github.therapi:therapi-runtime-javadoc:0.15.0")
-            dependency("com.github.therapi:therapi-runtime-javadoc-scribe:0.15.0")
-        }
-    }
 }
