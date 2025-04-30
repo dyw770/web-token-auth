8ed648129b5f5195742339ee387aeca264b9afb6
dyw770 <dyw770@qq.com>
2025-04-30 18:14:34 +0800

    增加跨域配置

--- auth-demo/src/main/java/cn/dyw/auth/demo/configuration/WebSecurityConfiguration.java
+++ auth-demo/src/main/java/cn/dyw/auth/demo/configuration/WebSecurityConfiguration.java
@@ -3,6 +3,11 @@ package cn.dyw.auth.demo.configuration;
 import cn.dyw.auth.core.AuthorizeHttpRequestsCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.UrlPathHelper;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * @author dyw770
@@ -14,11 +19,27 @@ public class WebSecurityConfiguration {
     @Bean
     public AuthorizeHttpRequestsCustomizer authorizeHttpRequestsCustomizer() throws Exception {
         return (authorize) -> authorize
-                .requestMatchers("/login").permitAll()
+                .requestMatchers("/user/login").permitAll()
                 .requestMatchers("/error").permitAll()
                 .requestMatchers("/doc/**").permitAll()
                 .requestMatchers("/favicon.ico").permitAll();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(PathPatternParser pathPatternParser, UrlPathHelper urlPathHelper) {
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(pathPatternParser);
+        source.setUrlPathHelper(urlPathHelper);
+        
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
