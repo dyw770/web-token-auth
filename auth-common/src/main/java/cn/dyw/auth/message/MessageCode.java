6964fb0ea2620d0d6f9e5140e6760d6e52bb45da
dyw770 <dyw770@qq.com>
2025-04-30 18:12:41 +0800

    调整错误码段，方便前台判断

--- auth-common/src/main/java/cn/dyw/auth/message/MessageCode.java
+++ auth-common/src/main/java/cn/dyw/auth/message/MessageCode.java
@@ -12,19 +12,21 @@ public enum MessageCode {
 
     ERROR(1, "请求失败"),
 
+    // 1000 - 1100 登录相关，这部分错误码前台需要直接退出
     NOT_LOGIN(1000, "未登录"),
-    NOT_PERMISSION(1001, "没有权限"),
     TOKEN_ERROR(1002, "token错误"),
     TOKEN_INVALID(1003, "token无效或已过期"),
     TOKEN_NOT_SET(1004, "token未设置"),
-    
-    BAD_CREDENTIALS(1005, "用户名或密码错误"),
-    
+
+    BAD_CREDENTIALS(1100, "用户名或密码错误"),
     
     PARAM_ERROR(2000, "参数错误"),
     
     REQUEST_METHOD_ERROR(2003, "请求方法错误"),
     REQUEST_BODY_ERROR(2004, "Request Body异常"),
+
+    // 3000 权限相关
+    NOT_PERMISSION(3001, "没有权限"),
     ;
 
 
