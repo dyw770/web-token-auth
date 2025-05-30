1d16b0223a69024541c07f670dadf6f1979e85da
dyw770 <dyw770@qq.com>
2025-05-30 17:54:41 +0800

    优化用户管理中给用户角色授权的操作

--- auth-ui/src/views/admin/user/auth.vue
+++ auth-ui/src/views/admin/user/auth.vue
@@ -1,18 +1,42 @@
 <template>
   <el-dialog v-model="show" title="授权" width="500px" @opened="getRoleList">
     <el-tree
-      ref="treeRef"
-      style="max-width: 600px"
+      ref="treeRoleRef"
       :data="treeData"
-      show-checkbox
       default-expand-all
       node-key="roleCode"
-      highlight-current
+      :expand-on-click-node="false"
       :props="defaultProps"
-    />
+    >
+      <template #default="{ node, data }">
+        <div
+          class="tree-node"
+          @mouseenter="data.showButton = true"
+          @mouseleave="data.showButton = false"
+        >
+              <span class="justify-center inline-flex items-center gap-2">
+                <FaIcon :name="data.checked ? 'ant-design:check-circle-filled' : 'ant-design:close-circle-filled'"
+                        size="4"/>
+                {{ node.label }}
+              </span>
+          <span v-show="data.showButton" class="tree-node-buttons">
+                <el-button-group>
+                  <el-button size="small" :type="data.checked ? 'danger' : 'success'" @click="auth(data)">
+                    {{ data.checked ? '取消授权' : '授权' }}
+                  </el-button>
+                </el-button-group>
+              </span>
+        </div>
+      </template>
+      <template #empty>
+        <div class="text-center">
+          <FaIcon name="ant-design:file-outlined" size="64px"/>
+          <p class="text-gray-500">请先再系统中创建角色</p>
+        </div>
+      </template>
+    </el-tree>
     <template #footer>
       <div class="dialog-footer">
-        <el-button @click="closeAuthDialog">取消</el-button>
         <el-button type="primary" @click="submitAuth">
           确认
         </el-button>
@@ -28,10 +52,20 @@ import type {TreeInstance} from 'element-plus'
 import type {Role, User} from "#/api";
 import {toast} from "vue-sonner";
 
+interface TreeRole extends Role.RoleListRs {
+  checked?: boolean
+  showButton?: boolean
+}
+
 // 控制授权弹窗显示
 const show = defineModel({required: true, type: Boolean})
 
 const {user} = defineProps<{ user: User.UserRs | undefined }>()
+
+
+watch(() => user, () => {
+  getRoleList()
+})
 
 const emit = defineEmits({
   success: () => {
@@ -41,18 +75,37 @@ const emit = defineEmits({
 
 const treeRef = ref<TreeInstance>()
 
-const treeData = ref<Role.RoleListRs[]>()
+const treeData = ref<TreeRole[]>([])
 
 const getRoleList = async () => {
   const {data} = await adminApi.roleList()
   treeData.value = data
+  buildTreeRole()
+}
 
-  if (user) {
-    const checkedKeys = user.roles.map(role => {
-      return role.roleCode
-    })
-    treeRef.value!.setCheckedKeys(checkedKeys)
+const buildTreeRole = () => {
+  if (!(user && user.roles)) {
+    return
   }
+  const roles = user.roles.map(item => item.roleCode)
+  treeData.value.forEach(item => {
+    checkRole(item, roles, undefined)
+  })
+}
+
+const checkRole  = (role: TreeRole, roles: string[], parentRole: TreeRole | undefined) => {
+  if (roles.includes(role.roleCode)) {
+    role.checked = true
+  }
+  if(role.parentRoleCode && roles.includes(role.parentRoleCode)) (
+    role.checked = true
+  )
+  if (parentRole && parentRole.checked) {
+    role.checked = true
+  }
+  role.children.forEach(item => {
+    checkRole(item, roles, role)
+  })
 }
 
 const closeAuthDialog = () => {
@@ -60,13 +113,14 @@ const closeAuthDialog = () => {
   treeRef.value!.setCheckedNodes([])
 }
 
-const submitAuth = async () => {
-  const checkedNodes = treeRef.value!.getCheckedNodes(false)
-  const roleCodes = checkedNodes.map(node => {
-    return node.roleCode
-  })
-  await adminApi.addRoleForUser(user!.username, roleCodes)
+const auth = async (role: TreeRole) => {
+  await adminApi.addRoleForUser(user!.username, [role.roleCode])
   toast.success('授权成功')
+  await getRoleList()
+  emit('success')
+}
+
+const submitAuth = async () => {
   closeAuthDialog()
   emit('success')
 }
@@ -80,5 +134,23 @@ const defaultProps = {
 
 
 <style scoped>
+.tree-node {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  padding: 8px 12px;
+  transition: background-color 0.2s ease;
+  width: 100%;
+}
 
+.tree-node-buttons {
+  display: inline-block;
+  opacity: 0.9;
+  transition: opacity 0.2s ease;
+}
+
+.el-tree {
+  --el-tree-node-content-height: 48px
+}
 </style>
