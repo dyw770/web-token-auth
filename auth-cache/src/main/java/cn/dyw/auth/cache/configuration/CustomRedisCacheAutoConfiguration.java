177410a607d11923ae64d923c12c5f6f3f4dccfd
dyw770 <dyw770@qq.com>
2025-06-09 22:22:04 +0800

    调整缓存配置

---
+++ auth-cache/src/main/java/cn/dyw/auth/cache/configuration/CustomRedisCacheAutoConfiguration.java
@@ -0,0 +1,223 @@
+package cn.dyw.auth.cache.configuration;
+
+import cn.dyw.auth.cache.CacheNames;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.TimeoutOptions;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import io.lettuce.core.resource.ClientResources;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientOptionsBuilderCustomizer;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.ssl.SslOptions;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.BatchStrategies;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.util.StringUtils;
+
+import java.time.Duration;
+import java.util.Collections;
+
+import static cn.dyw.auth.cache.configuration.RedisCacheAutoConfiguration.defaultConfiguration;
+
+/**
+ * @author dyw770
+ * @since 2025-06-09
+ */
+@EnableCaching
+@Configuration
+@EnableConfigurationProperties(AuthCacheProperties.class)
+@ConditionalOnProperty(name = "app.auth.cache.use-custom-redis", havingValue = "true")
+public class CustomRedisCacheAutoConfiguration extends RedisConnectionConfiguration {
+
+    private final AuthCacheProperties cacheProperties;
+
+    public CustomRedisCacheAutoConfiguration(AuthCacheProperties properties,
+                                             ObjectProvider<SslBundles> sslBundles) {
+        super(properties.getRedis(),
+                new PropertiesRedisConnectionDetails(properties.getRedis()),
+                sslBundles);
+        this.cacheProperties = properties;
+    }
+
+
+    @Bean
+    public RedisCacheManager cacheManager(
+            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
+            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
+            ClientResources clientResources) {
+
+        LettuceConnectionFactory connectionFactory = redisConnectionFactory(
+                clientConfigurationBuilderCustomizers,
+                clientOptionsBuilderCustomizers,
+                clientResources
+        );
+
+        connectionFactory.start();
+
+        RedisCacheWriter cacheWriter = RedisCacheWriter
+                .nonLockingRedisCacheWriter(connectionFactory, BatchStrategies.scan(1000));
+
+        return RedisCacheManager.builder(cacheWriter)
+                .cacheDefaults(defaultConfiguration(cacheProperties))
+                .transactionAware()
+                .initialCacheNames(Collections.singleton(CacheNames.ROLE_CACHE))
+                .build();
+    }
+
+
+    private LettuceConnectionFactory redisConnectionFactory(
+            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
+            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
+            ClientResources clientResources) {
+        return createConnectionFactory(clientConfigurationBuilderCustomizers, clientOptionsBuilderCustomizers,
+                clientResources);
+    }
+
+
+    private LettuceConnectionFactory createConnectionFactory(
+            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
+            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
+            ClientResources clientResources) {
+        LettuceClientConfiguration clientConfig = getLettuceClientConfiguration(clientConfigurationBuilderCustomizers,
+                clientOptionsBuilderCustomizers, clientResources, getProperties().getLettuce().getPool());
+        return createLettuceConnectionFactory(clientConfig);
+    }
+
+    private LettuceConnectionFactory createLettuceConnectionFactory(LettuceClientConfiguration clientConfiguration) {
+        if (getSentinelConfig() != null) {
+            return new LettuceConnectionFactory(getSentinelConfig(), clientConfiguration);
+        }
+        if (getClusterConfiguration() != null) {
+            return new LettuceConnectionFactory(getClusterConfiguration(), clientConfiguration);
+        }
+        return new LettuceConnectionFactory(getStandaloneConfig(), clientConfiguration);
+    }
+
+    private LettuceClientConfiguration getLettuceClientConfiguration(
+            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
+            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
+            ClientResources clientResources, RedisProperties.Pool pool) {
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = createBuilder(pool);
+        applyProperties(builder);
+        if (StringUtils.hasText(getProperties().getUrl())) {
+            customizeConfigurationFromUrl(builder);
+        }
+        builder.clientOptions(createClientOptions(clientOptionsBuilderCustomizers));
+        builder.clientResources(clientResources);
+        clientConfigurationBuilderCustomizers.orderedStream().forEach((customizer) -> customizer.customize(builder));
+        return builder.build();
+    }
+
+    private LettuceClientConfiguration.LettuceClientConfigurationBuilder createBuilder(RedisProperties.Pool pool) {
+        if (isPoolEnabled(pool)) {
+            return new PoolBuilderFactory().createBuilder(pool);
+        }
+        return LettuceClientConfiguration.builder();
+    }
+
+    private void applyProperties(LettuceClientConfiguration.LettuceClientConfigurationBuilder builder) {
+        if (isSslEnabled()) {
+            builder.useSsl();
+        }
+        if (getProperties().getTimeout() != null) {
+            builder.commandTimeout(getProperties().getTimeout());
+        }
+        if (getProperties().getLettuce() != null) {
+            RedisProperties.Lettuce lettuce = getProperties().getLettuce();
+            if (lettuce.getShutdownTimeout() != null && !lettuce.getShutdownTimeout().isZero()) {
+                builder.shutdownTimeout(getProperties().getLettuce().getShutdownTimeout());
+            }
+        }
+        if (StringUtils.hasText(getProperties().getClientName())) {
+            builder.clientName(getProperties().getClientName());
+        }
+    }
+
+    private ClientOptions createClientOptions(
+            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientConfigurationBuilderCustomizers) {
+        ClientOptions.Builder builder = initializeClientOptionsBuilder();
+        Duration connectTimeout = getProperties().getConnectTimeout();
+        if (connectTimeout != null) {
+            builder.socketOptions(SocketOptions.builder().connectTimeout(connectTimeout).build());
+        }
+        if (isSslEnabled() && getProperties().getSsl().getBundle() != null) {
+            SslBundle sslBundle = getSslBundles().getBundle(getProperties().getSsl().getBundle());
+            io.lettuce.core.SslOptions.Builder sslOptionsBuilder = io.lettuce.core.SslOptions.builder();
+            sslOptionsBuilder.keyManager(sslBundle.getManagers().getKeyManagerFactory());
+            sslOptionsBuilder.trustManager(sslBundle.getManagers().getTrustManagerFactory());
+            SslOptions sslOptions = sslBundle.getOptions();
+            if (sslOptions.getCiphers() != null) {
+                sslOptionsBuilder.cipherSuites(sslOptions.getCiphers());
+            }
+            if (sslOptions.getEnabledProtocols() != null) {
+                sslOptionsBuilder.protocols(sslOptions.getEnabledProtocols());
+            }
+            builder.sslOptions(sslOptionsBuilder.build());
+        }
+        builder.timeoutOptions(TimeoutOptions.enabled());
+        clientConfigurationBuilderCustomizers.orderedStream().forEach((customizer) -> customizer.customize(builder));
+        return builder.build();
+    }
+
+    private ClientOptions.Builder initializeClientOptionsBuilder() {
+        if (getProperties().getCluster() != null) {
+            ClusterClientOptions.Builder builder = ClusterClientOptions.builder();
+            RedisProperties.Lettuce.Cluster.Refresh refreshProperties = getProperties().getLettuce().getCluster().getRefresh();
+            ClusterTopologyRefreshOptions.Builder refreshBuilder = ClusterTopologyRefreshOptions.builder()
+                    .dynamicRefreshSources(refreshProperties.isDynamicRefreshSources());
+            if (refreshProperties.getPeriod() != null) {
+                refreshBuilder.enablePeriodicRefresh(refreshProperties.getPeriod());
+            }
+            if (refreshProperties.isAdaptive()) {
+                refreshBuilder.enableAllAdaptiveRefreshTriggers();
+            }
+            return builder.topologyRefreshOptions(refreshBuilder.build());
+        }
+        return ClientOptions.builder();
+    }
+
+    private void customizeConfigurationFromUrl(LettuceClientConfiguration.LettuceClientConfigurationBuilder builder) {
+        if (urlUsesSsl()) {
+            builder.useSsl();
+        }
+    }
+
+    /**
+     * Inner class to allow optional commons-pool2 dependency.
+     */
+    private static final class PoolBuilderFactory {
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder createBuilder(RedisProperties.Pool properties) {
+            return LettucePoolingClientConfiguration.builder().poolConfig(getPoolConfig(properties));
+        }
+
+        private GenericObjectPoolConfig<?> getPoolConfig(RedisProperties.Pool properties) {
+            GenericObjectPoolConfig<?> config = new GenericObjectPoolConfig<>();
+            config.setMaxTotal(properties.getMaxActive());
+            config.setMaxIdle(properties.getMaxIdle());
+            config.setMinIdle(properties.getMinIdle());
+            if (properties.getTimeBetweenEvictionRuns() != null) {
+                config.setTimeBetweenEvictionRuns(properties.getTimeBetweenEvictionRuns());
+            }
+            if (properties.getMaxWait() != null) {
+                config.setMaxWait(properties.getMaxWait());
+            }
+            return config;
+        }
+
+    }
+}
--- auth-cache/src/main/java/cn/dyw/auth/cache/configuration/RedisCacheAutoConfiguration.java
+++ auth-cache/src/main/java/cn/dyw/auth/cache/configuration/RedisCacheAutoConfiguration.java
@@ -2,22 +2,8 @@ package cn.dyw.auth.cache.configuration;
 
 import cn.dyw.auth.cache.CacheNames;
 import cn.dyw.auth.utils.ObjectMapperUtils;
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
-import io.lettuce.core.TimeoutOptions;
-import io.lettuce.core.cluster.ClusterClientOptions;
-import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
-import io.lettuce.core.resource.ClientResources;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientOptionsBuilderCustomizer;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.ssl.SslBundle;
-import org.springframework.boot.ssl.SslBundles;
-import org.springframework.boot.ssl.SslOptions;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,14 +12,9 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.util.StringUtils;
 
-import java.time.Duration;
 import java.util.Collections;
 
 /**
@@ -43,63 +24,31 @@ import java.util.Collections;
 @EnableCaching
 @Configuration
 @EnableConfigurationProperties(AuthCacheProperties.class)
-public class RedisCacheAutoConfiguration extends RedisConnectionConfiguration {
+@ConditionalOnProperty(name = "app.auth.cache.use-custom-redis", havingValue = "false", matchIfMissing = true)
+public class RedisCacheAutoConfiguration {
 
 
     private final AuthCacheProperties cacheProperties;
 
-    public RedisCacheAutoConfiguration(AuthCacheProperties properties,
-                                       ObjectProvider<SslBundles> sslBundles) {
-        super(properties.getRedis(),
-                new PropertiesRedisConnectionDetails(properties.getRedis()),
-                sslBundles);
+    public RedisCacheAutoConfiguration(AuthCacheProperties properties) {
         this.cacheProperties = properties;
     }
 
 
     @Bean
-    @ConditionalOnProperty(name = "app.auth.cache.use-custom-redis", havingValue = "false", matchIfMissing = true)
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 
         RedisCacheWriter cacheWriter = RedisCacheWriter
                 .nonLockingRedisCacheWriter(connectionFactory, BatchStrategies.scan(1000));
-
+        
         return RedisCacheManager.builder(cacheWriter)
-                .cacheDefaults(defaultConfiguration())
+                .cacheDefaults(defaultConfiguration(cacheProperties))
                 .transactionAware()
                 .initialCacheNames(Collections.singleton(CacheNames.ROLE_CACHE))
                 .build();
     }
 
-    @Configuration
-    @ConditionalOnProperty(name = "app.auth.cache.use-custom-redis", havingValue = "true")
-    public class CustomRedisCacheAutoConfiguration {
-        @Bean
-        public RedisCacheManager cacheManager(
-                ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
-                ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
-                ClientResources clientResources) {
-
-            LettuceConnectionFactory connectionFactory = redisConnectionFactory(
-                    clientConfigurationBuilderCustomizers,
-                    clientOptionsBuilderCustomizers,
-                    clientResources
-            );
-
-            connectionFactory.start();
-
-            RedisCacheWriter cacheWriter = RedisCacheWriter
-                    .nonLockingRedisCacheWriter(connectionFactory, BatchStrategies.scan(1000));
-
-            return RedisCacheManager.builder(cacheWriter)
-                    .cacheDefaults(defaultConfiguration())
-                    .transactionAware()
-                    .initialCacheNames(Collections.singleton(CacheNames.ROLE_CACHE))
-                    .build();
-        }
-    }
-
-    public RedisCacheConfiguration defaultConfiguration() {
+    public static RedisCacheConfiguration defaultConfiguration(AuthCacheProperties cacheProperties) {
 
         return RedisCacheConfiguration
                 .defaultCacheConfig()
@@ -110,148 +59,5 @@ public class RedisCacheAutoConfiguration extends RedisConnectionConfiguration {
                 .entryTtl(cacheProperties.getExpireTime())
                 .enableTimeToIdle()
                 .computePrefixWith(cacheName -> cacheProperties.getCachePrefix() + cacheName + ":");
-    }
-
-
-    private LettuceConnectionFactory redisConnectionFactory(
-            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
-            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
-            ClientResources clientResources) {
-        return createConnectionFactory(clientConfigurationBuilderCustomizers, clientOptionsBuilderCustomizers,
-                clientResources);
-    }
-
-
-    private LettuceConnectionFactory createConnectionFactory(
-            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
-            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
-            ClientResources clientResources) {
-        LettuceClientConfiguration clientConfig = getLettuceClientConfiguration(clientConfigurationBuilderCustomizers,
-                clientOptionsBuilderCustomizers, clientResources, getProperties().getLettuce().getPool());
-        return createLettuceConnectionFactory(clientConfig);
-    }
-
-    private LettuceConnectionFactory createLettuceConnectionFactory(LettuceClientConfiguration clientConfiguration) {
-        if (getSentinelConfig() != null) {
-            return new LettuceConnectionFactory(getSentinelConfig(), clientConfiguration);
-        }
-        if (getClusterConfiguration() != null) {
-            return new LettuceConnectionFactory(getClusterConfiguration(), clientConfiguration);
-        }
-        return new LettuceConnectionFactory(getStandaloneConfig(), clientConfiguration);
-    }
-
-    private LettuceClientConfiguration getLettuceClientConfiguration(
-            ObjectProvider<LettuceClientConfigurationBuilderCustomizer> clientConfigurationBuilderCustomizers,
-            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientOptionsBuilderCustomizers,
-            ClientResources clientResources, RedisProperties.Pool pool) {
-        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = createBuilder(pool);
-        applyProperties(builder);
-        if (StringUtils.hasText(getProperties().getUrl())) {
-            customizeConfigurationFromUrl(builder);
-        }
-        builder.clientOptions(createClientOptions(clientOptionsBuilderCustomizers));
-        builder.clientResources(clientResources);
-        clientConfigurationBuilderCustomizers.orderedStream().forEach((customizer) -> customizer.customize(builder));
-        return builder.build();
-    }
-
-    private LettuceClientConfiguration.LettuceClientConfigurationBuilder createBuilder(RedisProperties.Pool pool) {
-        if (isPoolEnabled(pool)) {
-            return new PoolBuilderFactory().createBuilder(pool);
-        }
-        return LettuceClientConfiguration.builder();
-    }
-
-    private void applyProperties(LettuceClientConfiguration.LettuceClientConfigurationBuilder builder) {
-        if (isSslEnabled()) {
-            builder.useSsl();
-        }
-        if (getProperties().getTimeout() != null) {
-            builder.commandTimeout(getProperties().getTimeout());
-        }
-        if (getProperties().getLettuce() != null) {
-            RedisProperties.Lettuce lettuce = getProperties().getLettuce();
-            if (lettuce.getShutdownTimeout() != null && !lettuce.getShutdownTimeout().isZero()) {
-                builder.shutdownTimeout(getProperties().getLettuce().getShutdownTimeout());
-            }
-        }
-        if (StringUtils.hasText(getProperties().getClientName())) {
-            builder.clientName(getProperties().getClientName());
-        }
-    }
-
-    private ClientOptions createClientOptions(
-            ObjectProvider<LettuceClientOptionsBuilderCustomizer> clientConfigurationBuilderCustomizers) {
-        ClientOptions.Builder builder = initializeClientOptionsBuilder();
-        Duration connectTimeout = getProperties().getConnectTimeout();
-        if (connectTimeout != null) {
-            builder.socketOptions(SocketOptions.builder().connectTimeout(connectTimeout).build());
-        }
-        if (isSslEnabled() && getProperties().getSsl().getBundle() != null) {
-            SslBundle sslBundle = getSslBundles().getBundle(getProperties().getSsl().getBundle());
-            io.lettuce.core.SslOptions.Builder sslOptionsBuilder = io.lettuce.core.SslOptions.builder();
-            sslOptionsBuilder.keyManager(sslBundle.getManagers().getKeyManagerFactory());
-            sslOptionsBuilder.trustManager(sslBundle.getManagers().getTrustManagerFactory());
-            SslOptions sslOptions = sslBundle.getOptions();
-            if (sslOptions.getCiphers() != null) {
-                sslOptionsBuilder.cipherSuites(sslOptions.getCiphers());
-            }
-            if (sslOptions.getEnabledProtocols() != null) {
-                sslOptionsBuilder.protocols(sslOptions.getEnabledProtocols());
-            }
-            builder.sslOptions(sslOptionsBuilder.build());
-        }
-        builder.timeoutOptions(TimeoutOptions.enabled());
-        clientConfigurationBuilderCustomizers.orderedStream().forEach((customizer) -> customizer.customize(builder));
-        return builder.build();
-    }
-
-    private ClientOptions.Builder initializeClientOptionsBuilder() {
-        if (getProperties().getCluster() != null) {
-            ClusterClientOptions.Builder builder = ClusterClientOptions.builder();
-            RedisProperties.Lettuce.Cluster.Refresh refreshProperties = getProperties().getLettuce().getCluster().getRefresh();
-            ClusterTopologyRefreshOptions.Builder refreshBuilder = ClusterTopologyRefreshOptions.builder()
-                    .dynamicRefreshSources(refreshProperties.isDynamicRefreshSources());
-            if (refreshProperties.getPeriod() != null) {
-                refreshBuilder.enablePeriodicRefresh(refreshProperties.getPeriod());
-            }
-            if (refreshProperties.isAdaptive()) {
-                refreshBuilder.enableAllAdaptiveRefreshTriggers();
-            }
-            return builder.topologyRefreshOptions(refreshBuilder.build());
-        }
-        return ClientOptions.builder();
-    }
-
-    private void customizeConfigurationFromUrl(LettuceClientConfiguration.LettuceClientConfigurationBuilder builder) {
-        if (urlUsesSsl()) {
-            builder.useSsl();
-        }
-    }
-
-    /**
-     * Inner class to allow optional commons-pool2 dependency.
-     */
-    private static final class PoolBuilderFactory {
-
-        LettuceClientConfiguration.LettuceClientConfigurationBuilder createBuilder(RedisProperties.Pool properties) {
-            return LettucePoolingClientConfiguration.builder().poolConfig(getPoolConfig(properties));
-        }
-
-        private GenericObjectPoolConfig<?> getPoolConfig(RedisProperties.Pool properties) {
-            GenericObjectPoolConfig<?> config = new GenericObjectPoolConfig<>();
-            config.setMaxTotal(properties.getMaxActive());
-            config.setMaxIdle(properties.getMaxIdle());
-            config.setMinIdle(properties.getMinIdle());
-            if (properties.getTimeBetweenEvictionRuns() != null) {
-                config.setTimeBetweenEvictionRuns(properties.getTimeBetweenEvictionRuns());
-            }
-            if (properties.getMaxWait() != null) {
-                config.setMaxWait(properties.getMaxWait());
-            }
-            return config;
-        }
-
     }
 }
